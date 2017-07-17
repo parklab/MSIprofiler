3f47db6c61da5ad33c5373e5901c6eca03f7397b
Scott Ouellette <scottx611x@gmail.com>
2017-07-17 16:32:39 -0400

    Remove dead code & formatting

--- get_reference_set_from_fasta.py
+++ get_reference_set_from_fasta.py
@@ -1,15 +1,9 @@
 
 import sys
-from sputnik_reference import find_repeats
 #add option for notation: ensemble (1,2,3) o ucsc (chr1, chr2,..)
+from utils import find_repeats
 
-
-#chr=int(sys.argv[1]) ##XX
-chr=sys.argv[1]
-#notation=sys.argv[2])
-#if notation not in ["Ensemble","UCSC"]:
-#    print "The second argument, correspoding to the chr notation, needs to be Ensemble or UCSC"
-#    raise
+chr = sys.argv[1]
 
 fi = "chrs_fa/chr"+str(chr)+".fa"
 
@@ -19,19 +13,30 @@ with open(fi, "rb") as myfile:
     sequence = ''.join([line.strip("\n").upper() for line in myfile.readlines()[1:]])
 
 
-o = find_repeats(sequence,0,chr,min_score=5)
-#del sequence
+o = find_repeats(sequence, 0, chr, min_score=5)
 
 ## XX add to REFINE THE SET
 ## add to find location
 
-f = open("reference_set_"+str(chr)+"_up_to_5rep_in_flank.txt",'w') 
+f = open("reference_set_"+str(chr)+"_up_to_5rep_in_flank.txt",'w')
 
 for ms in o:
-    repeats_flanking_left = find_repeats(sequence[ms[1]-10:ms[1]],0,chr,min_score=4)
-    repeats_flanking_right = find_repeats(sequence[ms[2]:ms[2]+10],0,chr,min_score=4)
+    repeats_flanking_left = find_repeats(
+        sequence[ms[1]-10:ms[1]],
+        0,
+        chr,
+        min_score=4
+    )
+    repeats_flanking_right = find_repeats(
+        sequence[ms[2]:ms[2]+10],
+        0,
+        chr,
+        min_score=4
+    )
     if len(repeats_flanking_left) == 0 and len(repeats_flanking_right) == 0:
-        cmd = str(ms[0]) + "\t" +str(ms[1]) + "\t" +str(ms[2])+ "\t"+str(ms[3]) + "\t"+str(ms[4])+ "\t" +str(ms[5]) #+ "\t"+sequence[ms[1]-8:ms[1]] + "\t" + sequence[ms[2]:ms[2]+8]
-        f.write(cmd+"\n")
+        cmd = "{}\t{}\t{}\t{}\t{}\t{}".format(
+            ms[0], ms[1], ms[2], ms[3], ms[4], ms[5]
+        )
+        f.write(cmd + "\n")
 
 f.close()
