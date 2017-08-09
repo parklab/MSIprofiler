4e3487a07711e1043e690d86791b4551d3e2c417
Scott Ouellette <scottx611x@gmail.com>
2017-08-09 15:55:09 -0400

    update get_reference_sets_from_fasta

--- scripts/get_reference_set_from_fasta.py
+++ scripts/get_reference_set_from_fasta.py
@@ -1,39 +1,107 @@
 import argparse
-from reps_from_reference  import find_repeats
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-parser = argparse.ArgumentParser(description='usage: get_reference_set_from_fasta.py --chr 1 --notation Ensemble --min_score 5')
-parser.add_argument('--chr', help='Chromosome',required=False,default=22)
-# Optional arguments
-parser.add_argument('--notation', help='Chromosome notation. The two options are: Ensemble (e.g. 1,2,3,..) or UCSC (e.g. chr1, chr2, chr3,..)',required=False, default="Ensemble",choices=["Ensemble","UCSC"])
-parser.add_argument('--min_score', help='Minimum score to detect microsatellites in the flanking regions. Default is 4.',required=False,default=5,type=int)
-parser.add_argument('--max_length', help='Maximum microsatellite length. Default is 60.',required=False,default=60,type=int)
-args = parser.parse_args()
-
-fi = "chrs_fa/chr"+str(args.chr)+".fa"
-
-#load fasta file:
-# we skip first line, as it contains the chr name
-with open(fi, "rb") as myfile:
-    sequence = ''.join([line.strip("\n").upper() for line in myfile.readlines()[1:]])
+from utils import find_repeats
 
 
-o = find_repeats(sequence,0,args.chr,min_score=args.min_score)
-#del sequence
+def initialize_parser():
+    parser = argparse.ArgumentParser(
+        description=(
+            'usage: get_reference_set_from_fasta.py '
+            '--chr 1 '
+            '--notation Ensemble '
+            '--min_score 5'
+        )
+    )
+    parser.add_argument(
+        '--chr',
+        help='Chromosome',
+        required=False,
+        default=22
+    )
 
-f = open("reference_set_"+str(args.chr)+".txt",'w') 
+    # Optional arguments
+    parser.add_argument(
+        '--notation',
+        help=(
+            'Chromosome notation. The two options are: Ensemble (e.g. 1,2,3,'
+              '..) or UCSC (e.g. chr1, chr2, chr3,..)'
+        ),
+        required=False,
+        default="Ensemble",
+        choices=["Ensemble","UCSC"]
+    )
+    parser.add_argument(
+        '--min_score',
+        help=(
+            'Minimum score to detect microsatellites in the flanking regions. '
+              'Default is 4.'
+        ),
+        required=False,
+        default=5,
+        type=int
+    )
+    parser.add_argument(
+        '--max_length',
+        help='Maximum microsatellite length. Default is 60.',
+        required=False,
+        default=60,
+        type=int
+    )
+    return parser
 
-for ms in o:
-    repeats_flanking_left = find_repeats(sequence[ms[1]-10:ms[1]],0,args.chr,min_score=args.min_score)
-    repeats_flanking_right = find_repeats(sequence[ms[2]:ms[2]+10],0,args.chr,min_score=args.min_score)
-    if len(repeats_flanking_left) == 0 and len(repeats_flanking_right) == 0:
-        # get the GC contents
-        region = sequence[ms[1]-1000:ms[2]+1000] 
-        GC = region.count("G") + region.count("C")
-        GC = round(GC / float(2000), 1)
-        if args.notation == "Ensemble":
-            cmd = str(ms[0]) + "\t" +str(ms[1]) + "\t" +str(ms[2])+ "\t"+str(ms[3]) + "\t"+str(ms[4])+ "\t" +str(ms[5]) +"\t"+str(GC)
-        else:
-            cmd = "chr"+str(ms[0]) + "\t" +str(ms[1]) + "\t" +str(ms[2])+ "\t"+str(ms[3]) + "\t"+str(ms[4])+ "\t" +str(ms[5]) + "\t"+ str(GC)
-        f.write(cmd+"\n")
 
-f.close()
+def main():
+    parser = initialize_parser()
+    args = parser.parse_args()
+
+    fi = "../chrs_fa/chr" + str(args.chr) + ".fa"
+
+    assert os.path.exists(fi), "File: {} doesn't exist.".format(fi)
+
+    #load fasta file:
+    # we skip first line, as it contains the chr name
+    with open(fi, "rb") as myfile:
+        sequence = ''.join(
+            [line.strip("\n").upper() for line in myfile.readlines()[1:]]
+        )
+    chromosome = int(args.chr)
+
+    o = find_repeats(sequence, 0, [chromosome], min_score=args.min_score)
+
+    with open("reference_set_"+str(chromosome)+".txt", 'w') as f:
+        for ms in o:
+            repeats_flanking_left = find_repeats(
+                sequence[ms[1]-10:ms[1]],
+                0,
+                [chromosome],
+                min_score=args.min_score
+            )
+            repeats_flanking_right = find_repeats(
+                sequence[ms[2]:ms[2]+10],
+                0,
+                [chromosome],
+                min_score=args.min_score
+            )
+            if len(repeats_flanking_left) == 0 and \
+                    len(repeats_flanking_right) == 0:
+                # get the GC contents
+                region = sequence[ms[1]-1000:ms[2]+1000]
+                GC = region.count("G") + region.count("C")
+                GC = round(GC / float(2000), 1)
+
+                base_cmd = "{}\t{}\t{}\t{}\t{}\t{}\t{}".format(
+                    ms[0], ms[1], ms[2], ms[3], ms[4], ms[5], GC
+                )
+
+                if args.notation == "Ensemble":
+                    cmd = base_cmd
+                else:
+                    cmd = "chr{}".format(base_cmd)
+
+                f.write(cmd+"\n")
+
+if __name__ == "__main__":
+    main()
